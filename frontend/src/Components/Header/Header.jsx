05873c860e68bc8b5d0d837abb27d55238f29b3c
VakaPrasanna <20BQ1A05O6@vvit.net>
2023-05-11 07:19:27 +0530

    logo Commit

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import { Container, Row, Button } from 'reactstrap';
+import { Navlink, Link} from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
+
+const nav_links=[
+  {
+    path:'/home',
+    display:'Home'
+  },
+  {
+    path:'#',
+    display:'About'
+  },
+  {
+    path:'/faculty',
+    display:'Faculty'
+  },
+]
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+      <div className="header">
+        <Container>
+          <Row>
+            <div className="nav_wrapper d-flex align-items-center justify-content-between">
+                <div className="logo">
+                  <img src={logo} alt="VVIT logo" />
+                </div>
+            </div>
+          </Row>
+        </Container>
+      </div>
   )
 };
 
